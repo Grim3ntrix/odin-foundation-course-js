2dc095ca011586fc0dfe5fcfee9e26a50e38d59e
Richard Samberi <richardsamberi101@gmail.com>
2024-11-27 00:00:42 +0800

    Add exercise-3

---
+++ exercises/02-data-types-and-conditionals/exercise-3/math.js
@@ -0,0 +1,12 @@
+/**
+ * Lets do some math!
+ * Some rules first:
+ *   - Replace the strings to the right of the = with the math expression they describe.
+ *   - Do not manually enter the answers to the equations. For example, `const a = 9` would be incorrect as 9 is the answer to the equation you're being asked to write out
+ */
+
+export const a = 1+8;
+export const b = 22*3;
+export const c = 5%4;
+export const d = a - 17;
+export const e = a+b+c+d;
