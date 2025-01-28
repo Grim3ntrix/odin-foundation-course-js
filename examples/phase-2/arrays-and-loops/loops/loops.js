c92a94750942e6d8e74e57b7f96dac349bd4baaf
Grim3ntrix <richardsamberi101@gmail.com>
2025-01-28 16:23:35 +0800

    Add filling in a guest active learning

--- examples/phase-2/arrays-and-loops/loops/loops.js
+++ examples/phase-2/arrays-and-loops/loops/loops.js
@@ -374,51 +374,43 @@ console.log("\n");
 console.log("| Active learning: Filling in a guest list");
 console.log("\n");
 console.log("In this exercise, we want you to take a list of names stored in an array and put them into a guest list. But it's not quite that easy — we don't want to let Phil and Lola in because they are greedy and rude, and always eat all the food! We have two lists, one for guests to admit, and one for guests to refuse.");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
-console.log("\n");
+
+const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
+
+const admitted = document.getElementById("admitted");
+const refused = document.getElementById("refused");
+
+admitted.textContent = 'Admit: ';
+refused.textContent = 'Refuse: ';
+
+for (const person of people) {
+  if (person === 'Phil' || person === 'Lola') {
+    refused.textContent += `${person}, `;
+
+
+  } else {
+    admitted.textContent += `${person}, `;
+
+  }
+}
+console.log("Admitted", admitted.textContent.length);
+console.log("Refused", refused.textContent.length);
+
+refused.textContent = refused.textContent.slice(0,refused.textContent.length-2) + '.';
+
+admitted.textContent = admitted.textContent.slice(0,admitted.textContent.length-2) + '.';
+
+
+// var peopleLen = 0;
+
+// while (peopleLen < people.length) {
+    
+//     if (people[peopleLen] === 'Phil') {
+//         refused.textContent += `${people[peopleLen]}, `;
+//     } else if (people[peopleLen] === 'Lola') {
+//         refused.textContent += `${people[peopleLen]}`;
+//     } else {
+//         admitted.textContent += `${people[peopleLen]}, `;
+//     }
+//     peopleLen++
+// }
